f438d797a8465f77a7223311bbd78f5e81bc0140
andrewc <andrewc@machven.com>
2016-01-12 17:34:12 +1000

    Initial Commit

---
+++ BananasSwiftNew/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  BananasSwiftNew
+//
+//  Created by Andrew on 1/12/16.
+//  Copyright (c) 2016 MachVen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
